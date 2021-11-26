e5d564f3a1da0476929b170aacc61a2ed5f93d4f
Andrew McNeil <andrew.richard.mcneil@gmail.com>
2021-11-26 09:00:52 -0800

    Updates

---
+++ precache-manifest.07179921e881d7f8de68749053deae0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-ux-portfolio/favicon.ico"
+  },
+  {
+    "revision": "0655ba0ddb56d29e4afb111db1fb3a56",
+    "url": "/elm-ux-portfolio/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-ux-portfolio/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-ux-portfolio/manifest.json"
+  },
+  {
+    "revision": "513d2164834e1d0ae09c",
+    "url": "/elm-ux-portfolio/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "513d2164834e1d0ae09c",
+    "url": "/elm-ux-portfolio/static/js/main.513d2164.chunk.js"
+  },
+  {
+    "revision": "fdb9ace01c0a27ff2a60",
+    "url": "/elm-ux-portfolio/static/js/runtime~main.fdb9ace0.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/elm-ux-portfolio/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
